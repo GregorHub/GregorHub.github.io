696e09810316464dbf6550c13deaea6420af0303
@s6a35634 <gregor.klaus.hubrich@students.hs-mainz.de>
2020-08-22 08:49:24 +0200

    update links

--- scroll.js
+++ scroll.js
@@ -1,5 +1,11 @@
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+      return document.querySelector('.navbar').classList.add('hide')
+  }
+  return document.querySelector('.navbar').classList.remove('hide')
 
+});
 
 function clickStart(){
   var elmnt = document.getElementById("About");
